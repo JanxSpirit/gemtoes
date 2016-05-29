1d694518639feb1f63d2b044a97cced768088038
JanxSpirit <greggcarrier@gmail.com>
2016-05-29 01:05:54 -0400

    refactor to use composure api/schemas

--- src/clj/gemtoes/handler.clj
+++ src/clj/gemtoes/handler.clj
@@ -1,6 +1,7 @@
 (ns gemtoes.handler
-  (:require [compojure.core :refer [GET POST defroutes]]
-            [compojure.route :refer [not-found resources]]
+  (:require [compojure.route :refer [not-found resources]]
+            [compojure.api.sweet :refer :all]
+            [schema.core :as s]
             [ring.util.http-response :as response]
             [hiccup.page :refer [include-js include-css html5]]
             [gemtoes.middleware :refer [wrap-middleware]]
@@ -9,11 +10,23 @@
             [monger.collection :as mc]
             [monger.conversion :as mconvert]))
 
+(s/defschema Gmto
+  {:title s/Str})
+
 (def conn (mg/connect))
 (def db   (mg/get-db conn "gemtoes"))
 
-(defn save-gmto [gmto] (mc/insert-and-return db "gmtos" gmto))
-(defn save-maker [maker] (mconvert/from-db-object (mc/insert-and-return db "makers" maker) true))
+(defn clean-object-id [dbo]
+  (-> dbo
+      (assoc :id (str (:_id dbo)))
+      (dissoc :_id)))
+
+(defn save-gmto [gmto] 
+  (clean-object-id (mc/insert-and-return db "gmtos" gmto)))
+
+(defn save-maker [maker] 
+  (clean-object-id (mc/insert-and-return db "makers" maker)))
+
 (defn get-makers [] (let [res (map #(dissoc % :_id) (mc/find-maps db "makers"))]
                       (println res)
                       res
@@ -29,9 +42,9 @@
 (def loading-page
   (html5
    [:head
-     [:meta {:charset "utf-8"}]
-     [:meta {:name "viewport"
-             :content "width=device-width, initial-scale=1"}]
+    [:meta {:charset "utf-8"}]
+    [:meta {:name    "viewport"
+            :content "width=device-width, initial-scale=1"}]
      (include-css (if (env :dev) "/css/bootstrap.css" "/css/bootstrap.min.css"))]
     [:body
      mount-target
@@ -39,19 +52,33 @@
      (include-js "/js/jquery.js")
      (include-js "/js/bootstrap.min.js")]))
 
-
-(defroutes routes
+(defroutes gtroutes
   (GET "/" [] loading-page)
   (GET "/about" [] loading-page)
   (GET "/admin" [] loading-page)
 
-  (GET "/api/gmtos" [] (response/ok {:body [{"name" "value"} {"thing1" "red" "thing2" "blue" "thing3" ["a" "b" "c"]}]}))
-  (GET "/api/makers" [] (response/ok {:body (get-makers)}))
-  (POST "/api/gmtos" request (response/created (str (save-gmto (request :params)))))
-  (POST "/api/makers" request (response/created (str (save-maker (request :params)))))
+  ;; makers
+  (context "/api" []
+           :tags ["api"]
+           (context "/gmtos" []
+                    :tags ["gmtos"]
+                    (GET "/" []
+                         :return {:result Gmto}
+                         :summary "Return list of GMTOs"
+                         (response/ok {:result {:title "My GMTO"}}))
+                    (POST "/" []
+                          :return s/Str
+                          :body [gmto Gmto]
+                          :summary "Add a GMTO"
+                          (response/created (save-gmto gmto))))
+          #_ (context "/makers"
+                      (GET "/" [] (response/ok {:body (get-makers)}))
+                    (POST "/" request (response/created (str (save-maker (request :params)))))))
 
   (resources "/")
   (not-found "Not Found")
   )
 
-(def app (wrap-middleware #'routes))
+(def app (wrap-middleware #'gtroutes))
+
+
